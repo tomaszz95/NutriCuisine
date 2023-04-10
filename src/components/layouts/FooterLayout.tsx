f13b0f5b233488af10e93c1da2d45903d07481dc
Tomasz Ż <tzuber95@gmail.com>
2023-04-10 16:00:16 +0200

    fixed active on nav

--- src/components/layouts/FooterLayout.tsx
+++ src/components/layouts/FooterLayout.tsx
@@ -5,19 +5,31 @@ import styles from './FooterLayout.module.css'
 const FooterLayout = () => {
   return (
     <nav className={styles.nav}>
-      <NavLink to="/menu">
+      <NavLink
+        to="/menu"
+        className={({ isActive }) => (isActive ? `${styles.active}` : '')}
+      >
         <i className="fa-solid fa-utensils"></i>
         <span>Menu</span>
       </NavLink>
-      <NavLink to="/ingredients">
+      <NavLink
+        to="/ingredients"
+        className={({ isActive }) => (isActive ? `${styles.active}` : '')}
+      >
         <i className="fa-solid fa-carrot"></i>
         <span>Ingredients</span>
       </NavLink>
-      <NavLink to="/favorites">
+      <NavLink
+        to="/favorites"
+        className={({ isActive }) => (isActive ? `${styles.active}` : '')}
+      >
         <i className="fa-solid fa-star"></i>
         <span>Favorites</span>
       </NavLink>
-      <NavLink to="/shopping_list">
+      <NavLink
+        to="/shopping_list"
+        className={({ isActive }) => (isActive ? `${styles.active}` : '')}
+      >
         <i className="fa-solid fa-cart-shopping"></i>
         <span>Shopping list</span>
       </NavLink>
